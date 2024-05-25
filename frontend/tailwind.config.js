048b1e8af0762b1806be72b7b76b9077cb24bd64
aleiwi <aleiwi9@gmail.com>
2024-05-25 19:43:03 +0300

    edit tailwindcss config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        black: "#000000",
+        mainRed: "#DB4444",
+        hoverRed: "#E07575",
+      },
+      fontFamily: {
+        poppins: ['"Poppins", sans-serif'],
+        inter: ['"Inter", sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
