29260fa5d9a89ebf21e1f3689d7cc078715c47d6
salmanabdellatif <70966096+salmanabdellatif@users.noreply.github.com>
2024-06-17 16:15:24 +0300

    db-product model

---
+++ backend/models/product.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'please provide name'],
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: [true, 'please provide category'],
+  },
+  images: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  desc: {
+    type: String,
+    required: true,
+  },
+  rate: {
+    type: Number,
+    min: 0,
+    max: 1,
+    default: 0,
+  },
+  mainPrice: {
+    type: Number,
+    required: true,
+  },
+  rateNumber: {
+    type: Number,
+    default: 0,
+  },
+  inStockQty: {
+    type: Number,
+    default: 0,
+  },
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+export default Product
