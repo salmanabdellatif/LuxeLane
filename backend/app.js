d29d0ebe2f07f7de27fc59b415dfae7e53b0bfb6
Rafe-zarka <aboalreef1999@gmail.com>
2024-06-17 16:49:08 +0300

    ADD MIDDILEWARES

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,11 @@ import errorHandlerMiddleware from './middleWare/error-handler.js'
 import notFoundMiddleware from './middleWare/not-found.js'
 
 // Routes
+import orderRoutes from './routes/orderRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import discountRoutes from './routes/discountRoutes.js'
+import creditCardRoutes from './routes/creditCardRoutes.js'
 
 dotenv.config()
 const app = express()
@@ -19,9 +22,11 @@ connectDB()
 app.get('/', (req, res) => {
   res.send('server is running...')
 })
-
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 app.use('/api/products', productRoutes)
 app.use('/api/discounts', discountRoutes)
+app.use('/api/creditCards', creditCardRoutes)
 
 // error handler middleware
 app.use(notFoundMiddleware)
